8bcc0835c1a801367c5eaa249877a90abf140b7a
ZauberNerd <zaubernerd@zaubernerd.de>
2016-09-25 23:29:20 +0200

    feat(branch): list all branches

--- source/branch.js
+++ source/branch.js
@@ -1,3 +1,28 @@
-export default function branch() {
-	throw new Error('git.branch is not yet implemented!');
+// @flow
+import type {Context, BranchCommands} from './index';
+
+const COMMANDS: { [key: BranchCommands]: BranchCommands } = {
+	LIST: 'LIST',
+	CREATE: 'CREATE',
+	DELETE: 'DELETE',
+	MOVE: 'MOVE'
+};
+
+export default async function branch(
+	context: Context,
+	name?: string,
+	newName?: string,
+	command?: BranchCommands = 'LIST'
+) {
+	const {client, state: {owner, repo}} = context;
+
+	const {default_branch} = await client.repos.get({user: owner, repo});
+	const branches = await client.repos.getBranches({user: owner, repo});
+
+	return branches.map(({name}) => {
+		return {
+			name,
+			default: name === default_branch
+		};
+	});
 }
