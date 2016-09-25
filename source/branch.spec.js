bb49f8cedf5d26c931e3dd79d48a2e23d3f5005b
ZauberNerd <zaubernerd@zaubernerd.de>
2016-09-25 23:29:51 +0200

    test(branch): add tests for branch creation

--- source/branch.spec.js
+++ source/branch.spec.js
@@ -18,12 +18,24 @@ test.beforeEach(t => {
 		.returnsPromise()
 		.resolves({default_branch: 'master'}); // eslint-disable-line camelcase
 
+	client.repos.getShaOfCommitRef = sinon
+		.stub(client.repos, 'getShaOfCommitRef')
+		.returnsPromise()
+		.resolves({sha: 'abcd1234'});
+
+	client.gitdata.createReference = sinon
+		.stub(client.gitdata, 'createReference')
+		.returnsPromise()
+		.resolves({});
+
 	t.context.client = client;
 });
 
 test.afterEach(t => {
 	t.context.client.repos.get.restore();
 	t.context.client.repos.getBranches.restore();
+	t.context.client.repos.getShaOfCommitRef.restore();
+	t.context.client.gitdata.createReference.restore();
 });
 
 test('calling without parameters', async t => {
@@ -38,5 +50,35 @@ test('calling without parameters', async t => {
 		const actualNames = actual.map(a => a.name);
 		const expectedNames = ['master', 'feature/git-github'];
 		t.deepEqual(actualNames, expectedNames, 'should list all branches');
+	}
+	{
+		const expected = [
+			{name: 'master', default: true},
+			{name: 'feature/git-github', default: false}
+		];
+		t.deepEqual(actual, expected, 'should find the default branch');
+	}
+});
+
+test('calling with one parameter (branch name)', async t => {
+	const client = t.context.client;
+	const context = {
+		client,
+		state: {owner: 'nerdlabs', repo: 'git-github', HEAD: 'master'}
+	};
+
+	await branch(context, 'my_new_branch');
+
+	{
+		const actual = client.gitdata.createReference.firstCall.args[0].ref;
+		const expected = 'refs/heads/my_new_branch';
+
+		t.is(actual, expected, 'should create a new branch with the correct ref');
+	}
+	{
+		const actual = client.gitdata.createReference.firstCall.args[0].sha;
+		const expected = 'abcd1234';
+
+		t.is(actual, expected, 'should branch off of HEAD');
 	}
 });
